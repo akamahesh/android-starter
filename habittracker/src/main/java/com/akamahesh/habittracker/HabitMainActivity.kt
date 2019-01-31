364014ed6bd6fc309120b13045016656af68b4c4
akamahesh <mckay1718@gmail.com>
2019-02-01 00:48:23 +0530

    initial commit

---
+++ habittracker/src/main/java/com/akamahesh/habittracker/HabitMainActivity.kt
@@ -0,0 +1,12 @@
+package com.akamahesh.habittracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HabitMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
